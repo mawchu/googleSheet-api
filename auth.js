16b74db44f31015180686653a2bd2fc907faf63d
mawchu <mawchu0412@gmail.com>
2022-08-08 15:32:40 +0800

    更新 token

--- auth.js
+++ auth.js
@@ -41,7 +41,7 @@ function getNewToken(oAuth2Client, callback) {
 * @param {function} callback 呼叫認證函式
 */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const {client_secret, client_id, redirect_uris} = credentials.web;
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
